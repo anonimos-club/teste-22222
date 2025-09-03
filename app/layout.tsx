b642db2138c0205dfb61313aa7dbe058835668a7
anonimos-club <fabiocruz12133@gmail.com>
2025-09-03 18:33:06 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,9 +9,9 @@ import { GameProvider } from "@/contexts/game-context"
 import { Suspense } from "react"
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.app",
+  title: "Raspa Premios",
+  description: "",
+  generator: "Raspa Premios",
 }
 
 export default function RootLayout({
